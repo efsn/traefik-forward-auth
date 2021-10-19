c4ffbee13e1f1d6d7aa4291265a989333583ccdd
arthur.chen <efsn.chan@gmail.com>
2021-10-19 14:30:20 +0800

    feat: add log definition

---
+++ internal/log.go
@@ -0,0 +1,45 @@
+package internal
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+var logger *logrus.Logger
+
+func NewDefaultLogger() *logrus.Logger {
+	logger = logrus.StandardLogger()
+	logrus.SetOutput(os.Stdout)
+
+	switch conf.LogFormat {
+	case "pretty":
+		break
+	case "json":
+		logrus.SetFormatter(&logrus.JSONFormatter{})
+	default:
+		logrus.SetFormatter(&logrus.TextFormatter{
+			DisableColors: true,
+			FullTimestamp: true,
+		})
+	}
+
+	switch conf.LogLevel {
+	case "trace":
+		logrus.SetLevel(logrus.TraceLevel)
+	case "debug":
+		logrus.SetLevel(logrus.DebugLevel)
+	case "info":
+		logrus.SetLevel(logrus.InfoLevel)
+	// case "warning":
+	case "error":
+		logrus.SetLevel(logrus.ErrorLevel)
+	case "fatal":
+		logrus.SetLevel(logrus.FatalLevel)
+	case "panic":
+		logrus.SetLevel(logrus.PanicLevel)
+	default:
+		logrus.SetLevel(logrus.WarnLevel)
+	}
+	return logger
+}
